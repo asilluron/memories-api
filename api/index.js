f5561f9840586a677e101fca87d95811b85d0c85
Andrew Silluron <asilluron@users.noreply.github.com>
2014-09-20 12:56:13 -0400

    s3upload file sigs

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 var userRoutes = require("./user");
 var loginRoutes = require("./login");
 var memoryRoutes = require("./memory");
+var momentFileSigRoute = require("./momentfilesig");
 
-module.exports = [].concat(userRoutes, loginRoutes, memoryRoutes);
+module.exports = [].concat(userRoutes, loginRoutes, memoryRoutes, momentFileSigRoute);
