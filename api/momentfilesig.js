6a04079681ae7338cc448a714adc054f143a4250
Andrew Silluron <asilluron@users.noreply.github.com>
2014-09-20 21:02:34 -0400

    prep upload for prod

--- api/momentfilesig.js
+++ api/momentfilesig.js
@@ -14,8 +14,14 @@ function fileSigApi(server) {
         config: {
             auth: 'jwt',
             handler: function(request, reply) {
+                var username = request.auth.credentials.username || request.auth.credentials.email;
                 var objName = request.query.s3_object_name;
-
+                var pubObjName = objName;
+                var msum = crypto.createHash('sha1');
+                msum.update(request.auth.credentials._id + "");
+                var prefix = msum.digest("hex");
+                objName = username + "/" + prefix + "_" + objName;
+                pubObjName = encodeURIComponent(encodeURIComponent(username)) + "/" + prefix + "_" + pubObjName;
                 var mimeType = request.query.s3_object_type;
 
                 var s3 = new AWS.S3();
@@ -26,7 +32,7 @@ function fileSigApi(server) {
                     ACL: "public-read"
                 };
                 var signedUrl = s3.getSignedUrl('putObject', params);
-                var publicUrl = 'https://' + process.env.S3_BUCKET + '.s3.amazonaws.com/' + objName;
+                var publicUrl = 'https://' + 's3-' +process.env.AWS_REGION +'.amazonaws.com/' + process.env.S3_BUCKET + "/" + objName;
                 var credentials = {
                     publicUrl: publicUrl,
                     signedUrl: signedUrl
